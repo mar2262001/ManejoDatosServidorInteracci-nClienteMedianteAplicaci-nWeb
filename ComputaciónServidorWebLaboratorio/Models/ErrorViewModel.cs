437cdfee3f1b7daa1bde4acb6d35d1a4f4855796
mar2262001 <mar2262001@gmail.com>
2023-02-02 18:47:43 -0600

    add template

---
+++ ComputaciónServidorWebLaboratorio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComputaciónServidorWebLaboratorio.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
